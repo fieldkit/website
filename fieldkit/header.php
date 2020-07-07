03547b55d586065abc996a28b2b2f6b7df574486
Bryan Wagman <bryanwagman@bryanwagman.com>
2020-07-06 23:53:34 -0400

    chore: add site header and footer files

---
+++ fieldkit/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+	<head>
+		<meta charset="<?php bloginfo('charset'); ?>" />
+		<meta content="width=device-width,initial-scale=1,viewport-fit=cover" name="viewport" />
+		<?php wp_head(); ?>
+	</head>
+	<body <?php body_class(); ?>>
+		<?php wp_body_open(); ?>
+		<div class="site-container">
+			<?php get_template_part('template-parts/layout/site-header'); ?>
--- fieldkit/template-parts/layout/site-header.php
+++ fieldkit/template-parts/layout/site-header.php
@@ -4,16 +4,9 @@ $header = get_field('header', 'option');
 <header class="site-header">
 	<div class="site-header__inner">
 		<div class="site-header__controls">
-			<button type="button" class="action-toggle-navigation site-header__control site-navigation-toggle">
-				<?php echo schwarzmanscholars_get_icon('menu', ['class' => 'site-navigation-toggle__open-icon']); ?>
-				<?php echo schwarzmanscholars_get_icon('close', ['class' => 'site-navigation-toggle__close-icon']); ?>
-			</button>
-			<button type="button" class="action-toggle-search site-header__control site-search-toggle">
-				<?php echo schwarzmanscholars_get_icon('search'); ?>
-			</button>
-			<button type="button" class="action-toggle-search site-header__control site-search-close">
-				<?php echo schwarzmanscholars_get_icon('close'); ?>
-			</button>
+			<button type="button" class="action-toggle-navigation site-header__control site-navigation-toggle"></button>
+			<button type="button" class="action-toggle-search site-header__control site-search-toggle"></button>
+			<button type="button" class="action-toggle-search site-header__control site-search-close"></button>
 		</div>
 		<div class="site-search"><?php get_search_form(); ?></div>
 		<nav class="site-navigation">
@@ -23,13 +16,5 @@ $header = get_field('header', 'option');
 			}
 			?>
 		</nav>
-		<?php
-		if ($call_to_action) {
-			$link = $call_to_action;
-			$link['class_name'] = 'site-header__call-to-action';
-			set_query_var('link', $link);
-			get_template_part('template-parts/utilities/link');
-		}
-		?>
 	</div>
 </header>
