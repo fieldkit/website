17cb5b86060bf68eb8796a64378e4cc9993558bb
Bryan Wagman <bryanwagman@bryanwagman.com>
2020-07-07 00:55:43 -0400

    chore: add mailchimp markup

---
+++ fieldkit/template-parts/components/mailchimp.php
@@ -0,0 +1,16 @@
+<div id="mc_embed_signup">
+	<form action="https://bryanwagman.us19.list-manage.com/subscribe/post?u=4d1fe390134f5139e55b006b6&amp;id=43db6d45e9" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+		<div id="mc_embed_signup_scroll">
+			<div class="mc-field-group">
+				<label for="mce-EMAIL">Email Address  <span class="asterisk">*</span></label>
+				<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+			</div>
+			<div id="mce-responses" class="clear">
+				<div class="response" id="mce-error-response" style="display:none"></div>
+				<div class="response" id="mce-success-response" style="display:none"></div>
+			</div>
+			<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_4d1fe390134f5139e55b006b6_43db6d45e9" tabindex="-1" value=""></div>
+			<button type="submit" name="subscribe" id="mc-embedded-subscribe" class="button">Subscribe</button>
+		</div>
+	</form>
+</div>
