9288a9f5d86735b1555141ebf20d60acb89a09bf
Joey Florentino <joey.moreno.florentino@gmail.com>
2020-07-08 17:52:07 +0800

    feat: sensor specs

---
+++ fieldkit/template-parts/blocks/image.php
@@ -0,0 +1,13 @@
+<section class="section section-image">
+	<div class="section__inner">
+		<div class="section-image__header">
+			<h2 class="heading-2">Schematics</h2>
+		</div>
+		<div class="section-image__body">
+			<p class="body">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut purus ligula, commodo quis mauris in, lacinia ultricies risus.</p>
+		</div>
+		<div class="section-image__image">
+			<img src="https://via.placeholder.com/1440x960" alt="">
+		</div>
+	</div>
+</section>
--- fieldkit/template-parts/blocks/introduction.php
+++ fieldkit/template-parts/blocks/introduction.php
@@ -1,4 +1,4 @@
-<!-- <header class="section section-introduction-header">
+<header class="section section-introduction-header">
 	<div class="section__inner">
 		<h2 class="heading-2">Sensor Specifications</h2>
 		<p class="body">A couple of sentences of a description of what the sensor is and how it operates within the broader FieldKit Ecosystem</p>
@@ -13,16 +13,4 @@
 			<p class="body">Basic Tech Specs can be included here on the web page. This information can serve as a clear indication that the sensors are of scientific quality. We do not want to hide the expert quality of these sensors.</p>
 		</div>
 	</div>
-</section> -->
-<?php
-    if( have_rows('content') ):
-    while ( have_rows('content') ) : the_row();
-    if( get_row_layout() == 'introduction' ):
-?>
-
-	<h2 class="heading-2"><?php get_sub_field('header_heading'); ?></h2>
-	<p class="body"><?php get_sub_field('header_body'); ?></p>
-
-<?php endif; endwhile; endif; ?>
-
-
+</section>
