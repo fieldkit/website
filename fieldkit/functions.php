682029dd8e967b2ba4de005c83eba59bb425586a
Bryan Wagman <bryanwagman@bryanwagman.com>
2020-07-08 23:59:14 -0400

    chore: enable woocommerce

--- fieldkit/functions.php
+++ fieldkit/functions.php
@@ -101,3 +101,8 @@ function fieldkit_get_icon($icon_name, $attributes = array())
 	$html .= ' alt="' . $icon_name . '" src="' . get_template_directory_uri() . '/assets/icons/' . $icon_name . '.svg" />';
 	return $html;
 }
+
+function fieldkit_add_woocommerce_support() {
+	add_theme_support('woocommerce');
+}
+add_action('after_setup_theme', 'fieldkit_add_woocommerce_support');
