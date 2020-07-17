bc63da355bde668b91711086c3e75b6ee2d1d41f
Angelo H. Lagumbay <iamthelosworld@gmail.com>
2020-07-17 15:59:12 +0800

    chore: optimize code and update acf

--- fieldkit/template-parts/blocks/rich-text.php
+++ fieldkit/template-parts/blocks/rich-text.php
@@ -1,8 +1,10 @@
 <?php
 $rich_text = get_sub_field('rich_text');
+$block_id = get_sub_field('block_id');
 ?>
 <section class="section section-rich-text">
 	<div class="section__inner">
+		<div id="<?php echo $block_id; ?>"></div>
 		<div class="rich-text"><?php echo $rich_text; ?></div>
 	</div>
 </section>
