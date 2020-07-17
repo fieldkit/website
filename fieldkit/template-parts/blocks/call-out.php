cad31a7d0c276a2432613f0defb3fa147c7f7313
Charles Medina <medinacharlesdan@gmail.com>
2020-07-18 05:52:55 +0800

    chore: call out refactor

--- fieldkit/template-parts/blocks/call-out.php
+++ fieldkit/template-parts/blocks/call-out.php
@@ -3,27 +3,25 @@ $heading = $call_out['heading'];
 $body = $call_out['body'];
 $links = $call_out['links'];
 ?>
-<header class="section section-header">
-	<div class="section__inner">
+<header class="section section-home-callout">
+	<div class="section__inner section__inner--inset">
 		<div class="rich-text">
 			<h2 class="heading-2"><?php echo $heading; ?></h2>
 			<?php echo $body; ?>
 		</div>
 		<?php if ($links) : ?>
-			<div class="section-header__links">
-				<?php
-				foreach ($links as $link_item) :
-					$link = $link_item['link'];
-				?>
-					<div class="section-header__links-item">
-						<?php
-						$link['class_name'] = 'button button--primary';
-						set_query_var('link', $link);
-						get_template_part('template-parts/utilities/link');
-						?>
-					</div>
-				<?php endforeach; ?>
-			</div>
+			<?php
+			foreach ($links as $link_item) :
+				$link = $link_item['link'];
+			?>
+				<div class="section-header__links-item">
+					<?php
+					$link['class_name'] = 'button button--primary';
+					set_query_var('link', $link);
+					get_template_part('template-parts/utilities/link');
+					?>
+				</div>
+			<?php endforeach; ?>
 		<?php endif; ?>
 	</div>
 </header>
