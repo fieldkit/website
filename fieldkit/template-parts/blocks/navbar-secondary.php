a280646f3bfaabe55923c6b0a0f4a8cb20264060
Charles Medina <medinacharlesdan@gmail.com>
2020-07-20 22:26:19 +0800

    chore: current ci

--- fieldkit/template-parts/blocks/navbar-secondary.php
+++ fieldkit/template-parts/blocks/navbar-secondary.php
@@ -1,10 +1,10 @@
-<section class="section section-navbar--secondary">
+<section class="section section-navbar--secondary" id='product-listing'>
 <div class="section__inner">
 <ul class="menu-navbar--secondary">
 <?php
 foreach($category as $cat){
 	printf(
-		__( '<li class="menu-item"> <a class="menu-link" href="%1$s"> %2$s </a> </li>' , 'all' ),
+		__( '<li class="menu-item"> <a class="menu-link" href="%1$s#product-listing"> %2$s </a> </li>' , 'all' ),
 		esc_html(get_category_link($cat->term_id)),
 		esc_html($cat->name)
 	);
