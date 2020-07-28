27aa4017f5fd6e9bb15e8f8ac5ef560f424e7588
Bryan Wagman <bryanwagman@bryanwagman.com>
2020-07-28 17:17:26 -0400

    chore: fix heading hierarchy of about page

--- fieldkit/templates/about.php
+++ fieldkit/templates/about.php
@@ -23,7 +23,7 @@
 	?>
 	<section class="section section-about-text-and-image-grid">
 		<div class="section__inner">
-			<header class="section-about-text-and-image-grid__header rich-text">
+			<header class="section-about-text-and-image-grid__header rich-text rich-text--heading-4">
 				<h2 class="heading-2"><?php echo $header_heading; ?></h2>
 				<?php echo $header_body; ?>
 			</header>
@@ -35,7 +35,7 @@
 					$image = $item['image'];
 				?>
 					<li class="section-about-text-and-image-grid__item">
-						<div class="section-about-text-and-image-grid__item-text rich-text">
+						<div class="section-about-text-and-image-grid__item-text rich-text rich-text--heading-5">
 							<h3 class="heading-3"><?php echo $heading; ?></h3>
 							<?php echo $body; ?>
 						</div>
@@ -56,7 +56,7 @@
 	<section class="section section-about-values">
 		<div class="section__inner section__inner--inset">
 			<header class="section-about-values__header">
-				<h3 class="heading-3"><?php echo $header_heading; ?></h3>
+				<h2 class="heading-3"><?php echo $header_heading; ?></h2>
 			</header>
 			<ul class="section-about-values__list">
 				<?php
@@ -68,7 +68,7 @@
 						<div class="section-about-values__icon">
 							<?php echo wp_get_attachment_image($icon['ID'], 'full'); ?>
 						</div>
-						<h4 class="heading-4"><?php echo $text; ?></h4>
+						<h3 class="section-about-values__item-text heading-4"><?php echo $text; ?></h3>
 					</li>
 				<?php endforeach; ?>
 			</ul>
