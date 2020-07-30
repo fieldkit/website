37d4c745289550e2c5a57ef2e9fdbf88a0b8657f
Charles Medina <medinacharlesdan@gmail.com>
2020-07-30 21:03:03 +0800

    style: margins and padding update

--- fieldkit/template-parts/components/teaser-product.php
+++ fieldkit/template-parts/components/teaser-product.php
@@ -5,6 +5,5 @@
         </div>
         <h3 class="heading-5 teaser-product__title"><?php the_title(); ?></h3>
         <div class="body teaser-product__excerpt"><?php the_excerpt(); ?></div>
-        <div class="heading-5 teaser-product__price">$xx.xx</div>
     </a>
 </div>
