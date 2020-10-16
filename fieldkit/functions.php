5637c1d2bdd38d5f311d61627bfe2fb9694ef849
Charles Medina <medinacharlesdan@gmail.com>
2020-10-16 12:19:29 +0800

    style: mobile cart

--- fieldkit/functions.php
+++ fieldkit/functions.php
@@ -220,6 +220,17 @@ function fieldkit_sku(){
 }
 
 
+remove_action( 'woocommerce_before_shop_loop_item_title', 'woocommerce_template_loop_product_thumbnail', 10 );
+add_action( 'woocommerce_before_shop_loop_item_title', 'fieldkit_loop_product_thumbnail', 10 );
+function fieldkit_loop_product_thumbnail() {
+    global $product;
+    $size = 'woocommerce_full';
+
+    $image_size = apply_filters( 'single_product_archive_thumbnail_size', $size );
+
+    echo $product ? $product->get_image( $image_size ) : '';
+}
+
 
 // add_action( 'woocommerce_payment_complete', 'order_received_empty_cart_action', 10, 1 );
 // function order_received_empty_cart_action( $order_id ){
