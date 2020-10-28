d29d00f02cf4e4d9f2079953609d1f9a2d0bdc95
Charles Medina <medinacharlesdan@gmail.com>
2020-10-29 01:20:13 +0800

    chore: acf field to wsiwyg

--- fieldkit/functions.php
+++ fieldkit/functions.php
@@ -242,7 +242,7 @@ remove_action( 'woocommerce_before_shop_loop_item_title', 'woocommerce_template_
 add_action( 'woocommerce_before_shop_loop_item_title', 'fieldkit_loop_product_thumbnail', 10 );
 function fieldkit_loop_product_thumbnail() {
     global $product;
-    $size = 'woocommerce_single';
+    $size = array(335, 195,true);
 
     $image_size = apply_filters( 'single_product_archive_thumbnail_size', $size );
 
