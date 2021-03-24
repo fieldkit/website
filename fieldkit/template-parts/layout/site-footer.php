ccfc29910c04c4579dc154dff01386add182e515
Joey Florentino <joey.moreno.florentino@gmail.com>
2021-03-24 13:03:29 +0800

    fix: restructure footer

--- fieldkit/template-parts/layout/site-footer.php
+++ fieldkit/template-parts/layout/site-footer.php
@@ -5,24 +5,37 @@ $facebook = $social['facebook'];
 $instagram = $social['instagram'];
 $twitter = $social['twitter'];
 $github = $social['github'];
+$youtube = $social['youtube'];
+$download_app = get_field('download_app', 'option');
+$app_store = $download_app['app_store'];
+$google_play = $download_app['google_play'];
 ?>
 <footer class="site-footer">
-	<div class="site-footer__inner">
-		<div class="site-footer__left">
-			<?php if ($dark_logo_id) : ?>
-				<div class="site-footer__logo">
-					<a href="<?php echo home_url('/'); ?>">
-						<?php echo wp_get_attachment_image($dark_logo_id, 'full'); ?>
-					</a>
-				</div>
-			<?php endif; ?>
-			<div class="site-footer__social">
-				<?php if ($twitter) : ?><a href="<?php echo $twitter; ?>" class="icon-button" target="_blank"><?php echo fieldkit_get_icon('twitter'); ?></a><?php endif; ?>
-				<?php if ($instagram) : ?><a href="<?php echo $instagram; ?>" class="icon-button" target="_blank"><?php echo fieldkit_get_icon('instagram'); ?></a><?php endif; ?>
-				<?php if ($facebook) : ?><a href="<?php echo $facebook; ?>" class="icon-button" target="_blank"><?php echo fieldkit_get_icon('facebook'); ?></a><?php endif; ?>
-				<?php if ($github) : ?><a href="<?php echo $github; ?>" class="icon-button" target="_blank"><?php echo fieldkit_get_icon('github'); ?></a><?php endif; ?>
-			</div>
+	<div class="site-footer__newsletter">
 			<?php get_template_part('template-parts/components/mailchimp'); ?>
+		</div>
+	<div class="site-footer__inner">
+
+		<div class="site-footer__left">
+			<div class="site-footer__social">
+				<?php if ($twitter || $instagram || $facebook || $github) :?>
+				<h2 class="body site-footer__social-heading">Connect with Us</h2>
+				<div class="site-footer__social-icons">
+					<?php if ($twitter) : ?><a href="<?php echo $twitter; ?>" class="icon-button" target="_blank"><?php echo fieldkit_get_icon('twitter'); ?></a><?php endif; ?>
+					<?php if ($instagram) : ?><a href="<?php echo $instagram; ?>" class="icon-button" target="_blank"><?php echo fieldkit_get_icon('instagram'); ?></a><?php endif; ?>
+					<?php if ($facebook) : ?><a href="<?php echo $facebook; ?>" class="icon-button" target="_blank"><?php echo fieldkit_get_icon('facebook'); ?></a><?php endif; ?>
+					<?php if ($github) : ?><a href="<?php echo $github; ?>" class="icon-button" target="_blank"><?php echo fieldkit_get_icon('github'); ?></a><?php endif; ?>
+					<?php if ($youtube) : ?><a href="<?php echo $youtube; ?>" class="icon-button" target="_blank"><?php echo fieldkit_get_icon('youtube'); ?></a><?php endif; ?>
+				</div>
+				<?php endif; ?>
+				<?php if ($app_store || $google_play) :?>
+				<h2 class="body site-footer__social-heading">Download the App</h2>
+				<div class="site-footer__download-app">
+					<?php if ($app_store) : ?><a href="<?php echo $app_store; ?>" class="app-icon" target="_blank"><?php echo fieldkit_get_icon('app_store'); ?></a><?php endif; ?>
+					<?php if ($google_play) : ?><a href="<?php echo $google_play; ?>" class="app-icon" target="_blank"><?php echo fieldkit_get_icon('google_play'); ?></a><?php endif; ?>
+				</div>
+				<?php endif; ?>
+			</div>
 		</div>
 		<div class="site-footer__right">
 			<div class="site-footer__right-column">
