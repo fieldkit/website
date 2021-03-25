1d3809e9dbf4e2edffad35cf7efc16e7001a174b
Joey Florentino <joey.moreno.florentino@gmail.com>
2021-03-25 13:43:50 +0800

    feat: cta-block

---
+++ fieldkit/template-parts/blocks/cta-block.php
@@ -0,0 +1,22 @@
+<?php
+$heading = get_sub_field('heading');
+$body = get_sub_field('body');
+$link = get_sub_field('link');
+$background_color = get_sub_field('background_color');
+?>
+
+<section class="section section-cta-block" style="background-color: <?php echo $background_color; ?>">
+	<div class="section__inner section__inner--inset">
+		<div class="rich-text rich-text--large">
+			<h2 class="heading-3"><?php echo $heading; ?></h2>
+			<?php if ($body) echo $body; ?>
+		</div>
+		<?php
+		if ($link) {
+			$link['class_name'] = 'button button--secondary section-cta-block__link';
+			set_query_var('link', $link);
+			get_template_part('template-parts/utilities/link');
+		}
+		?>
+	</div>
+</section>
