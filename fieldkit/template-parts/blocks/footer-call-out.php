4baa164ddc59f5c4e5f70f4539aa587947b2f337
Nards Rementilla <rementilla.nards@gmail.com>
2021-05-31 15:02:58 +0800

    fix:background visibility if fields are empty

--- fieldkit/template-parts/blocks/footer-call-out.php
+++ fieldkit/template-parts/blocks/footer-call-out.php
@@ -3,11 +3,12 @@ $heading = $footer_call_out['heading'];
 $body = $footer_call_out['body'];
 $link = $footer_call_out['link'];
 ?>
+<?php if($heading && $body): ?>
 <section class="section section-services-callout">
 	<div class="section__inner section__inner--inset">
 		<div class="rich-text rich-text--large">
 			<h2 class="heading-3"><?php echo $heading; ?></h2>
-			<?php if ($body) echo $body; ?>
+			<?php echo $body; ?>
 		</div>
 		<?php
 		if ($link) {
@@ -18,3 +19,4 @@ $link = $footer_call_out['link'];
 		?>
 	</div>
 </section>
+<?php endif; ?>
