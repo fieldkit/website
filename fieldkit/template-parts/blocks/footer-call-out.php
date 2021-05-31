17de6cf0cf51982fb8c6339348d615a6c5cf1067
Nards Rementilla <rementilla.nards@gmail.com>
2021-05-31 15:22:01 +0800

    fix:updating code for if conditions

--- fieldkit/template-parts/blocks/footer-call-out.php
+++ fieldkit/template-parts/blocks/footer-call-out.php
@@ -3,13 +3,17 @@ $heading = $footer_call_out['heading'];
 $body = $footer_call_out['body'];
 $link = $footer_call_out['link'];
 ?>
-<?php if($heading && $body): ?>
+<?php if($heading || $body || $link): ?>
 <section class="section section-services-callout">
 	<div class="section__inner section__inner--inset">
-		<div class="rich-text rich-text--large">
-			<h2 class="heading-3"><?php echo $heading; ?></h2>
-			<?php echo $body; ?>
-		</div>
+		<?php if($heading || $body) : ?>
+			<div class="rich-text rich-text--large">
+				<?php if($heading) : ?>
+				<h2 class="heading-3"><?php echo $heading; ?></h2>
+				<?php endif; ?>
+				<?php if ($body) echo $body; ?>
+			</div>
+		<?php endif; ?>
 		<?php
 		if ($link) {
 			$link['class_name'] = 'button--primary section-services-callout__link';
