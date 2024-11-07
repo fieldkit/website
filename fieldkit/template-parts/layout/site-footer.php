2caa68fa0e31c2f86f29faed7d26ea331d7544d9
Charles Medina <medinacharlesdan@gmail.com>
2024-11-07 23:01:55 +0800

    feat: gitlab theme setting

--- fieldkit/template-parts/layout/site-footer.php
+++ fieldkit/template-parts/layout/site-footer.php
@@ -6,38 +6,41 @@ $instagram = $social['instagram'];
 $twitter = $social['twitter'];
 $github = $social['github'];
 $youtube = $social['youtube'];
+$gitlab = $social['gitlab'];
 $download_app = get_field('download_app', 'option');
 $app_store = $download_app['app_store'];
 $google_play = $download_app['google_play'];
 ?>
 <footer class="site-footer">
-	<?php if(is_page('product-guide') || mv_is_subpage('product-guide')) : ?>
-	<button class="scroll-to-top" aria-label="scroll to top"></button>
+	<?php if (is_page('product-guide') || mv_is_subpage('product-guide')) : ?>
+		<button class="scroll-to-top" aria-label="scroll to top"></button>
 	<?php endif; ?>
 	<div class="site-footer__newsletter">
-			<?php get_template_part('template-parts/components/mailchimp'); ?>
-		</div>
+		<?php get_template_part('template-parts/components/mailchimp'); ?>
+	</div>
 	<div class="site-footer__inner">
 
 		<div class="site-footer__left">
 			<div class="site-footer__logo"><?php the_custom_logo(); ?></div>
 			<div class="site-footer__social">
-				<?php if ($twitter || $instagram || $facebook || $github) :?>
-				<h2 class="body site-footer__social-heading"><?php esc_html_e( 'Connect with Us', 'fieldkit' ); ?></h2>
-				<div class="site-footer__social-icons">
-					<?php if ($twitter) : ?><a href="<?php echo $twitter; ?>" class="icon-button" target="_blank"><?php echo fieldkit_get_icon('twitter'); ?></a><?php endif; ?>
-					<?php if ($instagram) : ?><a href="<?php echo $instagram; ?>" class="icon-button" target="_blank"><?php echo fieldkit_get_icon('instagram'); ?></a><?php endif; ?>
-					<?php if ($facebook) : ?><a href="<?php echo $facebook; ?>" class="icon-button" target="_blank"><?php echo fieldkit_get_icon('facebook'); ?></a><?php endif; ?>
-					<?php if ($github) : ?><a href="<?php echo $github; ?>" class="icon-button" target="_blank"><?php echo fieldkit_get_icon('github'); ?></a><?php endif; ?>
-					<?php if ($youtube) : ?><a href="<?php echo $youtube; ?>" class="icon-button" target="_blank"><?php echo fieldkit_get_icon('youtube'); ?></a><?php endif; ?>
-				</div>
+				<?php if ($twitter || $instagram || $facebook || $github) : ?>
+					<h2 class="body site-footer__social-heading"><?php esc_html_e('Connect with Us', 'fieldkit'); ?></h2>
+					<div class="site-footer__social-icons">
+						<?php if ($twitter) : ?><a href="<?php echo $twitter; ?>" class="icon-button" target="_blank"><?php echo fieldkit_get_icon('twitter'); ?></a><?php endif; ?>
+						<?php if ($instagram) : ?><a href="<?php echo $instagram; ?>" class="icon-button" target="_blank"><?php echo fieldkit_get_icon('instagram'); ?></a><?php endif; ?>
+						<?php if ($facebook) : ?><a href="<?php echo $facebook; ?>" class="icon-button" target="_blank"><?php echo fieldkit_get_icon('facebook'); ?></a><?php endif; ?>
+						<?php if ($github) : ?><a href="<?php echo $github; ?>" class="icon-button" target="_blank"><?php echo fieldkit_get_icon('github'); ?></a><?php endif; ?>
+						<?php if ($gitlab) : ?><a href="<?php echo $gitlab; ?>" class="icon-button" target="_blank"><?php echo fieldkit_get_icon('gitlab'); ?></a><?php endif; ?>
+						<?php if ($youtube) : ?><a href="<?php echo $youtube; ?>" class="icon-button" target="_blank"><?php echo fieldkit_get_icon('youtube'); ?></a><?php endif; ?>
+
+					</div>
 				<?php endif; ?>
-				<?php if ($app_store || $google_play) :?>
-				<h2 class="body site-footer__social-heading"><?php esc_html_e( 'Download the App', 'fieldkit' ); ?></h2>
-				<div class="site-footer__download-app">
-					<?php if ($app_store) : ?><a href="<?php echo $app_store; ?>" class="app-icon" target="_blank"><?php echo fieldkit_get_icon('app_store'); ?></a><?php endif; ?>
-					<?php if ($google_play) : ?><a href="<?php echo $google_play; ?>" class="app-icon" target="_blank"><?php echo fieldkit_get_icon('google_play'); ?></a><?php endif; ?>
-				</div>
+				<?php if ($app_store || $google_play) : ?>
+					<h2 class="body site-footer__social-heading"><?php esc_html_e('Download the App', 'fieldkit'); ?></h2>
+					<div class="site-footer__download-app">
+						<?php if ($app_store) : ?><a href="<?php echo $app_store; ?>" class="app-icon" target="_blank"><?php echo fieldkit_get_icon('app_store'); ?></a><?php endif; ?>
+						<?php if ($google_play) : ?><a href="<?php echo $google_play; ?>" class="app-icon" target="_blank"><?php echo fieldkit_get_icon('google_play'); ?></a><?php endif; ?>
+					</div>
 				<?php endif; ?>
 			</div>
 		</div>
