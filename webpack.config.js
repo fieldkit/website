2cca3c6541d73cb7c468a8de553ede71eb364eb9
Charles Medina <medinacharlesdan@gmail.com>
2025-03-04 18:02:11 +0800

    build: fix webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const webpack = require("webpack");
 const PACKAGE = require("./package.json");
 const version = PACKAGE.version;
@@ -7,41 +8,63 @@ const ProjectName = PACKAGE.name;
 
 
 module.exports = {
-  entry: "./fieldkit/assets/scripts/main.js",
+  entry: ["./fieldkit/assets/scripts/main.js",
+    "./fieldkit/assets/styles/main.scss",
+  ],
   module: {
     rules: [
       {
         generator: {
-          filename: "../fonts/fieldkit-fonts/[name].[contenthash][ext]",
+          filename: "./fieldkit/assets/fonts/fieldkit-fonts/[name].[contenthash][ext]",
         },
         test: /\.(woff|woff2|eot|ttf|otf)$/,
         type: "asset/resource",
       },
       {
         exclude: /node_modules/,
-        test: /\.js$/,
-        use: "babel-loader",
+        test: /\.(?:js|mjs|cjs)$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
       },
       {
-        test: /\.scss$/i,
+        test: /\.(css|sass|scss)$/,
         use: [
-          MiniCssExtractPlugin.loader, // Extracts CSS into a separate file
-          "css-loader", // Converts CSS into JS modules
-          "sass-loader", // Compiles SCSS to CSS
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: { importLoaders: 2, url: false },
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [["postcss-preset-env"]],
+              },
+            },
+          },
+          "sass-loader",
         ],
       },
     ],
   },
+  optimization: {
+    minimizer: ["...", new CssMinimizerPlugin()],
+  },
   output: {
-    filename: "main.bundle.js",
-    path: path.resolve(__dirname, "fieldkit/assets/scripts/"), // JS output path
+    filename: "assets/scripts/[name].bundle.js",
+    path: path.resolve(__dirname, "fieldkit"),
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: "../../style.css",
+      filename: "style.css",
     }),
     new webpack.BannerPlugin({
-      banner: `${ProjectName} v${version} - Copyright ${new Date().getUTCFullYear()}`,
+      banner: `Theme Name: ${ProjectName}\nText Domain: ${ProjectName}\nVersion: v${version}\nCopyright ${new Date().getUTCFullYear()}`,
+      test: /\.(css|sass|scss)$/,
     }),
   ],
 };
